d3cc49823c37f50e99738a5377a4e39a4751ceaa
May <mayxcai@gmail.com>
2018-04-27 13:11:32 -0400

    add tweeks to UI, fix timestamp function, edit README.md file

--- README.md
+++ README.md
@@ -11,7 +11,17 @@ This repository is the starter code for the project: Students will fork and clon
 3. Start the web server using the `npm run local` command. The app will be served at <http://localhost:8080/>.
 4. Go to <http://localhost:8080/> in your browser.
 
+
+## Screenshots
+
+!["Screenshot of compose tweet"](URL)
+!["Screenshot of posted tweets"](URL)
+
 ## Dependencies
 
-- Express
-- Node 5.10.x or above
+- express
+- node 5.10.x or above
+- body-parser
+- chance
+- md5
+- mongodb
--- public/scripts/timestamp-calc.js
+++ public/scripts/timestamp-calc.js
@@ -1,21 +1,26 @@
-function postTime(data) {
+function postTime(timestamp) {
 
   let currentDate = Date.now(); // returns date now in ms
-  let timestamp = data['created_at'];
   let timeDiff = currentDate - timestamp;
 
-  if (Math.floor(timeDiff / (1000 * 60 * 60 * 24 * 365) > 0)) {
+  if (Math.floor(timeDiff / (1000 * 60 * 60 * 24 * 365)) > 0) {
     return Math.floor(timeDiff / (1000 * 60 * 60 * 24 * 365)) + " years ago";
+
   } else if (Math.floor(timeDiff / (1000 * 60 * 60 * 24 * 30)) > 0) {
     return Math.floor(timeDiff / (1000 * 60 * 60 * 24 * 30)) + " months ago";
+
   } else if (Math.floor(timeDiff / (1000 * 60 * 60 * 24 * 24)) > 0) {
     return Math.floor(timeDiff / (1000 * 60 * 60 * 24)) + " days ago";
-  } else if (Math.floor(timeDiff / (1000 * 60 * 60) > 0)) {
+
+  } else if (Math.floor(timeDiff / (1000 * 60 * 60)) > 0) {
     return Math.floor(timeDiff / (1000 * 60 * 60)) + " hours ago";
-  } else if(Math.floor(timeDiff / (1000 * 60) > 0 )) {
-    return Math.floor(timeDiff / 1000 * 60) + " minutes ago";
-  } else if (Math.floor(timeDiff / 1000 > 0)) {
+
+  } else if(Math.floor(timeDiff / (1000 * 60)) > 0) {
+    return Math.floor(timeDiff / (1000 * 60)) + " minutes ago";
+
+  } else if (Math.floor(timeDiff / 1000) > 0) {
     return Math.floor(timeDiff / 1000) + " seconds ago";
+
   } else {
     return "0 seconds ago";
   }
